f22961b6d19e4cdf1e867bd7e4eb0663abf7ea74
Hungry_munk <abbaszadeharsam@gmail.com>
2022-11-06 07:31:45 +1100

    fields are now made valid or inavliad

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 const nameInput = document.getElementById('name');
-const EmailInput = document.getElementById('email');
+const emailInput = document.getElementById('email');
 const numberInput = document.getElementById('number');
 const passwordInput = document.getElementById('password');
 const passwordConformationInput = document.getElementById('password-conformation');
 
 const submitButton = document.querySelector('.button-container')
 
-const NAME_REGEX = new RegExp(/^[A-Z][a-z]+$/)
+const NAME_REGEX = new RegExp(/^[A-Z]+[a-z]+$/)
 const EMAIL_REGEX = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.com$/)
 const MELBOURNE_PHONE_REGEX = new RegExp (/^(04[0-9]{8}){1}$/)
 const PASSWORD_REGEX = new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%-_^&*\.])[a-zA-Z0-9!@#$%-_^&*\.]{8,25}$/)
 
  const fieldsPatterns = {
     name: [nameInput,NAME_REGEX],
-    email : [EmailInput, EMAIL_REGEX],
-    password:[passwordConformationInput, PASSWORD_REGEX],
-    number: [number, MELBOURNE_PHONE_REGEX]
- }
+    email: [emailInput, EMAIL_REGEX],
+    number: [number, MELBOURNE_PHONE_REGEX],
+    password: [passwordInput, PASSWORD_REGEX],
+    passwordConformation: [passwordConformationInput, PASSWORD_REGEX],
+}
+
+nameInput.addEventListener('input', ()=>{delayedValidationCheck('name')});
+emailInput.addEventListener('input', ()=>{delayedValidationCheck('email')});
+numberInput.addEventListener('input', ()=>{delayedValidationCheck('number')});
+passwordInput.addEventListener('input', ()=>{delayedValidationCheck('password')});
+
+
+let timer;
+let typingTimer;
+let typingTimerInterval = 1000
+
+
+
+function testValues (fieldValue,regex) {
+    return regex.test(fieldValue)
+};
+
+function delayedValidationCheck(fieldString) {
+    clearTimeout(typingTimer);
+    typingTimer = setTimeout(()=>{
+        if (testValues(fieldsPatterns[fieldString][0].value,fieldsPatterns[fieldString][1])) {
+            fieldsPatterns[fieldString][0].setCustomValidity('')
+        } else {
+            fieldsPatterns[fieldString][0].setCustomValidity('invalid')
+        }
+    }, typingTimerInterval);
+
+
+}
