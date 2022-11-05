6c3c0774f8ba81f7a038dd57c7dc9e7b71e59a7f
Hungry_munk <abbaszadeharsam@gmail.com>
2022-11-06 08:39:59 +1100

    error messagesn now show up properly

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ const passwordConformationInput = document.getElementById('password-conformation
 
 const submitButton = document.querySelector('.button-container')
 
-const NAME_REGEX = new RegExp(/^[A-Z]+[a-z]+$/)
+const NAME_REGEX = new RegExp(/^[A-Z]([a-z]+)?$/)
 const EMAIL_REGEX = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.com$/)
 const MELBOURNE_PHONE_REGEX = new RegExp (/^(04[0-9]{8}){1}$/)
 const PASSWORD_REGEX = new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%-_^&*\.])[a-zA-Z0-9!@#$%-_^&*\.]{8,25}$/)
 
-const nameErrorMessage = "name must start with a capital and be at least 2 characters";
+const nameErrorMessage = "name must start with a capital and then lowercase";
 const emailErrorMessage = "invalid email"
 const numberErrorMessage = "Must start with 04 and be 10 digits total"
 const passwordErrorMessage = "must have a digit, special character and be between 8 - 25 characters"
@@ -24,16 +24,27 @@ const passwordErrorMessage = "must have a digit, special character and be betwee
     passwordConformation: [passwordConformationInput, PASSWORD_REGEX],
 }
 
-nameInput.addEventListener('input', ()=>{delayedValidationCheck('name')});
-emailInput.addEventListener('input', ()=>{delayedValidationCheck('email')});
-numberInput.addEventListener('input', ()=>{delayedValidationCheck('number')});
-passwordInput.addEventListener('input', ()=>{delayedValidationCheck('password')});
-
-
 let timer;
 let typingTimer;
+let typingTimer2;
 let typingTimerInterval = 1000
 
+nameInput.addEventListener('input', (e)=>{
+    delayedValidationCheck(e.target.id)
+    displayError(e.target.id)
+});
+emailInput.addEventListener('input', (e)=>{
+    delayedValidationCheck(e.target.id)
+    displayError(e.target.id)
+});
+passwordInput.addEventListener('input', (e)=>{
+    delayedValidationCheck(e.target.id)
+    displayError(e.target.id)
+});
+numberInput.addEventListener('input', (e)=>{
+    delayedValidationCheck(e.target.id)
+    displayError(e.target.id)
+});
 
 
 function testValues (fieldValue,regex) {
@@ -42,21 +53,21 @@ function testValues (fieldValue,regex) {
 
 function delayedValidationCheck(fieldString) {
     clearTimeout(typingTimer);
-    const field = fieldsPatterns[fieldString][0];
-    const errorMessageSpan = document.querySelector(`#${field.id} ~ span`)
-
+    const field = fieldsPatterns[fieldString][0]
     typingTimer = setTimeout(()=>{
-        if (testValues(field.value,fieldsPatterns[fieldString][1])) {
-            field.setCustomValidity('')//field is now valid
-            errorMessageSpan.textContent = ' '
-        } else {
-            field.setCustomValidity('invalid')//field is now invalid
-            errorMessageSpan.textContent = fieldsPatterns[fieldString][2]
-
-            
-
-        }
+        if (testValues(field.value, fieldsPatterns[fieldString][1]) 
+            || (fieldString = "number" && !field.value)) //becuase number is not manditory
+                    field.setCustomValidity('')//field is now valid
+        else field.setCustomValidity('invalid')//field is now invalid
     }, typingTimerInterval);
+};
 
-
-}
+function displayError (fieldString) {
+    const field = fieldsPatterns[fieldString][0];
+    const errorSpan = document.querySelector(`#${field.id} ~ span`);
+    clearTimeout(typingTimer2);
+    typingTimer2 = setTimeout(()=>{
+        if (field.validity.valid || !field.value) errorSpan.textContent = " "
+        else errorSpan.textContent = fieldsPatterns[fieldString][2]        
+    },typingTimerInterval);
+};
