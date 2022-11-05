60ac82a45347939e3652d831e62f65977a3f4a30
Hungry_munk <abbaszadeharsam@gmail.com>
2022-11-06 07:54:44 +1100

    error messages show up butstill neeed fixing

--- script.js
+++ script.js
@@ -11,11 +11,16 @@ const EMAIL_REGEX = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]
 const MELBOURNE_PHONE_REGEX = new RegExp (/^(04[0-9]{8}){1}$/)
 const PASSWORD_REGEX = new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%-_^&*\.])[a-zA-Z0-9!@#$%-_^&*\.]{8,25}$/)
 
+const nameErrorMessage = "name must start with a capital and be at least 2 characters";
+const emailErrorMessage = "invalid email"
+const numberErrorMessage = "Must start with 04 and be 10 digits total"
+const passwordErrorMessage = "must have a digit, special character and be between 8 - 25 characters"
+
  const fieldsPatterns = {
-    name: [nameInput,NAME_REGEX],
-    email: [emailInput, EMAIL_REGEX],
-    number: [number, MELBOURNE_PHONE_REGEX],
-    password: [passwordInput, PASSWORD_REGEX],
+    name: [nameInput,NAME_REGEX, nameErrorMessage],
+    email: [emailInput, EMAIL_REGEX, emailErrorMessage],
+    number: [number, MELBOURNE_PHONE_REGEX, numberErrorMessage],
+    password: [passwordInput, PASSWORD_REGEX, passwordErrorMessage],
     passwordConformation: [passwordConformationInput, PASSWORD_REGEX],
 }
 
@@ -37,11 +42,19 @@ function testValues (fieldValue,regex) {
 
 function delayedValidationCheck(fieldString) {
     clearTimeout(typingTimer);
+    const field = fieldsPatterns[fieldString][0];
+    const errorMessageSpan = document.querySelector(`#${field.id} ~ span`)
+
     typingTimer = setTimeout(()=>{
-        if (testValues(fieldsPatterns[fieldString][0].value,fieldsPatterns[fieldString][1])) {
-            fieldsPatterns[fieldString][0].setCustomValidity('')
+        if (testValues(field.value,fieldsPatterns[fieldString][1])) {
+            field.setCustomValidity('')//field is now valid
+            errorMessageSpan.textContent = ' '
         } else {
-            fieldsPatterns[fieldString][0].setCustomValidity('invalid')
+            field.setCustomValidity('invalid')//field is now invalid
+            errorMessageSpan.textContent = fieldsPatterns[fieldString][2]
+
+            
+
         }
     }, typingTimerInterval);
 
