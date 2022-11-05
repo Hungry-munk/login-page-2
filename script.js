e79111e57ba56dd09466908cd7a430ff81823b2e
Hungry_munk <abbaszadeharsam@gmail.com>
2022-11-05 19:50:36 +1100

    added input field variables along with their regex

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-const 
+const nameInput = document.getElementById('name');
+const EmailInput = document.getElementById('email');
+const numberInput = document.getElementById('number');
+const passwordInput = document.getElementById('password');
+const passwordConformationInput = document.getElementById('password-conformation');
+
+const submitButton = document.querySelector('.button-container')
+
+const NAME_REGEX = new RegExp(/^[A-Z][a-z]+$/)
+const EMAIL_REGEX = new RegExp(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+\.com$/)
+const MELBOURNE_PHONE_REGEX = new RegExp (/^(04[0-9]{8}){1}$/)
+const PASSWORD_REGEX = new RegExp(/^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[!@#$%-_^&*\.])[a-zA-Z0-9!@#$%-_^&*\.]{8,25}$/)
+
+ const fieldsPatterns = {
+    name: [nameInput,NAME_REGEX],
+    email : [EmailInput, EMAIL_REGEX],
+    password:[passwordConformationInput, PASSWORD_REGEX],
+    number: [number, MELBOURNE_PHONE_REGEX]
+ }
